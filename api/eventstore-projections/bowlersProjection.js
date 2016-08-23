8429a04006fa39a344e55ff5ed3e2a8649fc5b27
Bradley Scott <bradley.s.scott@gmail.com>
2016-08-23 20:13:26 +1000

    Tried to ensure matchesProjection uploads correctly

--- api/eventstore-projections/bowlersProjection.js
+++ api/eventstore-projections/bowlersProjection.js
@@ -1,6 +1,6 @@
 fromStream('cricket_events_v1')
 .when({
     'cricket_event' : function(state ,event) {
-      if(event.data.bowler.id) linkTo('cricd-bowler-' + event.data.bowler.id, event);
+      if(event.data.bowler.id) { linkTo('cricd-bowler-' + event.data.bowler.id, event); }
     }
 });
